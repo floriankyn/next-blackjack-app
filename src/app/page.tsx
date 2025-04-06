0f883bef87556aaf7a7c276c332de1889aad6e42
Florian Lepage <florian.kyn@gmail.com>
2025-04-06 13:38:57 +0200

    Update ui main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,8 +7,10 @@ import HeroDescription from "@/components/molecules/HeroDescription";
 export default function Home() {
   return (
     <main>
-      <div className=" w-full h-[200px] bg-black flex justify-center items-center">
-        <h1 className="text-8xl font-bold text-white">Welcome to Blackjack!</h1> 
+      <div className=" w-full h-[250px] bg-black flex justify-center items-center">
+        <h1 className="text-8xl font-bold text-white text-center">
+          Welcome to <br /> Blackjack!
+        </h1> 
       </div>
       <div className="flex flex-col items-center mt-2 text-center">
         <div className="flex justify-center items-center w-full mt-4">
@@ -18,10 +20,10 @@ export default function Home() {
           <Rules />
         </div>     
 
-        <div>
+        <div className="mb-6">
           <Link href="/game">
-            <Button variant={"default"} className="mt-6 w-[200px]">
-              Play
+            <Button variant={"default"} className="mt-6 py-6 px-10 hover:scale-105 ease-in-out duration-300" size={"lg"}>
+              Play some Blackjack!
             </Button>
           </Link>
         </div>
