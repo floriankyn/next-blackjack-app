6b0553796be51d576e97017692d0b6db1a58d3c8
Florian Lepage <florian.kyn@gmail.com>
2025-04-06 13:38:14 +0200

    Add deck card change

---
+++ src/lib/constants.ts
@@ -0,0 +1,366 @@
+export const deck = [
+  {
+    value: "A",
+    name: "Ace",
+    suit: "Hearts",
+    color: "red",
+    image: "/images/blackjack/cards/ace_of_hearts.svg",
+  },
+  {
+    value: "2",
+    name: "Two",
+    suit: "Hearts",
+    color: "red",
+    image: "/images/blackjack/cards/2_of_hearts.svg",
+  }, 
+  {
+    value: "3",
+    name: "Three",
+    suit: "Hearts",
+    color: "red",
+    image: "/images/blackjack/cards/3_of_hearts.svg",
+  },
+  {
+    value: "4",
+    name: "Four",
+    suit: "Hearts",
+    color: "red",
+    image: "/images/blackjack/cards/4_of_hearts.svg",
+  },
+  {
+    value: "5",
+    name: "Five",
+    suit: "Hearts",
+    color: "red",
+    image: "/images/blackjack/cards/5_of_hearts.svg",
+  },
+  {
+    value: "6",
+    name: "Six",
+    suit: "Hearts",
+    color: "red",
+    image: "/images/blackjack/cards/6_of_hearts.svg",
+  },
+  {
+    value: "7",
+    name: "Seven",
+    suit: "Hearts",
+    color: "red",
+    image: "/images/blackjack/cards/7_of_hearts.svg",
+  },
+  {
+    value: "8",
+    name: "Eight",
+    suit: "Hearts",
+    color: "red",
+    image: "/images/blackjack/cards/8_of_hearts.svg",
+  },
+  {
+    value: "9",
+    name: "Nine",
+    suit: "Hearts",
+    color: "red",
+    image: "/images/blackjack/cards/9_of_hearts.svg",
+  },
+  {
+    value: "10",
+    name: "Ten",
+    suit: "Hearts",
+    color: "red",
+    image: "/images/blackjack/cards/10_of_hearts.svg"
+  },
+  {
+      value:"J", 
+      name:"Jack", 
+      suit:"Hearts", 
+      color:"red", 
+      image:"/images/blackjack/cards/jack_of_hearts.svg"
+  },
+  {
+      value:"Q", 
+      name:"Queen", 
+      suit:"Hearts", 
+      color:"red", 
+      image:"/images/blackjack/cards/queen_of_hearts.svg"
+  },
+  {
+      value:"K", 
+      name:"King", 
+      suit:"Hearts", 
+      color:"red", 
+      image:"/images/blackjack/cards/king_of_hearts.svg"
+  },
+  {
+    value: "A",
+    name: "Ace",
+    suit: "Diamonds",
+    color: "red",
+    image: "/images/blackjack/cards/ace_of_diamonds.svg",
+  },
+  {
+    value: "2",
+    name: "Two",
+    suit: "Diamonds",
+    color: "red",
+    image: "/images/blackjack/cards/2_of_diamonds.svg",
+  },
+  {
+    value: "3",
+    name: "Three",
+    suit: "Diamonds",
+    color: "red",
+    image: "/images/blackjack/cards/3_of_diamonds.svg",
+  },
+  {
+    value: "4",
+    name: "Four",
+    suit: "Diamonds",
+    color: "red",
+    image: "/images/blackjack/cards/4_of_diamonds.svg",
+  },
+  {
+    value: "5",
+    name: "Five",
+    suit: "Diamonds",
+    color: "red",
+    image: "/images/blackjack/cards/5_of_diamonds.svg",
+  },
+  {
+    value: "6",
+    name: "Six",
+    suit: "Diamonds",
+    color: "red",
+    image: "/images/blackjack/cards/6_of_diamonds.svg",
+  },
+  {
+      value:"7", 
+      name:"Seven", 
+      suit:"Diamonds", 
+      color:"red", 
+      image:"/images/blackjack/cards/7_of_diamonds.svg"
+  },
+  {
+      value:"8", 
+      name:"Eight", 
+      suit:"Diamonds", 
+      color:"red", 
+      image:"/images/blackjack/cards/8_of_diamonds.svg"
+  },
+  {
+      value:"9", 
+      name:"Nine", 
+      suit:"Diamonds", 
+      color:"red", 
+      image:"/images/blackjack/cards/9_of_diamonds.svg"
+  },
+  {
+      value:"10", 
+      name:"Ten", 
+      suit:"Diamonds", 
+      color:"red", 
+      image:"/images/blackjack/cards/10_of_diamonds.svg"
+  },
+  {
+      value:"J", 
+      name:"Jack", 
+      suit:"Diamonds", 
+      color:"red", 
+      image:"/images/blackjack/cards/jack_of_diamonds.svg"
+  },
+  {
+      value:"Q", 
+      name:"Queen", 
+      suit:"Diamonds", 
+      color:"red", 
+      image:"/images/blackjack/cards/queen_of_diamonds.svg"
+  },
+  {
+    value:"K", 
+    name:"King", 
+    suit:"Diamonds", 
+    color:"red", 
+    image:"/images/blackjack/cards/king_of_diamonds.svg"
+  },
+  {
+    value: "A",
+    name: "Ace",
+    suit: "Clubs",
+    color: "black",
+    image: "/images/blackjack/cards/ace_of_clubs.svg",
+  },
+  {
+    value: "2",
+    name: "Two",
+    suit: "Clubs",
+    color: "black",
+    image: "/images/blackjack/cards/2_of_clubs.svg",
+  },
+  {
+    value: "3",
+    name: "Three",
+    suit: "Clubs",
+    color: "black",
+    image: "/images/blackjack/cards/3_of_clubs.svg",
+  },
+  {
+    value: "4",
+    name: "Four",
+    suit: "Clubs",
+    color: "black",
+    image: "/images/blackjack/cards/4_of_clubs.svg",
+  },
+  {
+    value: "5",
+    name: "Five",
+    suit: "Clubs",
+    color: "black",
+    image: "/images/blackjack/cards/5_of_clubs.svg",
+  },
+  {
+      value:"6", 
+      name:"Six", 
+      suit:"Clubs", 
+      color:"black", 
+      image:"/images/blackjack/cards/6_of_clubs.svg"
+  },
+  {
+      value:"7", 
+      name:"Seven", 
+      suit:"Clubs", 
+      color:"black", 
+      image:"/images/blackjack/cards/7_of_clubs.svg"
+  },
+  {
+      value:"8", 
+      name:"Eight", 
+      suit:"Clubs", 
+      color:"black", 
+      image:"/images/blackjack/cards/8_of_clubs.svg"
+  },
+  {
+      value:"9", 
+      name:"Nine", 
+      suit:"Clubs", 
+      color:"black", 
+      image:"/images/blackjack/cards/9_of_clubs.svg"
+  },
+  {
+      value:"10", 
+      name:"Ten", 
+      suit:"Clubs", 
+      color:"black", 
+      image:"/images/blackjack/cards/10_of_clubs.svg"
+  },
+  {
+      value: "J",
+      name: "Jack",
+      suit: "Clubs",
+      color: "black",
+      image: "/images/blackjack/cards/jack_of_clubs.svg",
+  },
+  {
+      value:"Q", 
+      name:"Queen", 
+      suit:"Clubs", 
+      color:"black", 
+      image:"/images/blackjack/cards/queen_of_clubs.svg"
+  },
+  {
+    value:"K", 
+    name:"King", 
+    suit:"Clubs", 
+    color:"black", 
+    image:"/images/blackjack/cards/king_of_clubs.svg"
+  },
+  {
+    value: "A",
+    name: "Ace",
+    suit: "Spades",
+    color: "black",
+    image: "/images/blackjack/cards/ace_of_spades.svg",
+  },
+  {
+    value: "2",
+    name: "Two",
+    suit: "Spades",
+    color: "black",
+    image: "/images/blackjack/cards/2_of_spades.svg",
+  },
+  {
+      value:"3", 
+      name:"Three", 
+      suit:"Spades", 
+      color:"black", 
+      image:"/images/blackjack/cards/3_of_spades.svg"
+  },
+  {
+      value:"4", 
+      name:"Four", 
+      suit:"Spades", 
+      color:"black", 
+      image:"/images/blackjack/cards/4_of_spades.svg"
+  },
+  {
+      value:"5", 
+      name:"Five", 
+      suit:"Spades", 
+      color:"black", 
+      image:"/images/blackjack/cards/5_of_spades.svg"
+  },
+  {
+      value:"6", 
+      name:"Six", 
+      suit:"Spades", 
+      color:"black", 
+      image:"/images/blackjack/cards/6_of_spades.svg"
+  },
+  {
+      value:"7", 
+      name:"Seven", 
+      suit:"Spades", 
+      color:"black", 
+      image:"/images/blackjack/cards/7_of_spades.svg"
+  },
+  {
+      value:"8", 
+      name:"Eight", 
+      suit:"Spades", 
+      color:"black", 
+      image:"/images/blackjack/cards/8_of_spades.svg"
+  },
+  {
+    value: "9",
+    name: "Nine",
+    suit: "Spades",
+    color: "black",
+    image: "/images/blackjack/cards/9_of_spades.svg",
+  },
+  {
+    value: "10",
+    name: "Ten",
+    suit: "Spades",
+    color: "black",
+    image: "/images/blackjack/cards/10_of_spades.svg",
+  },
+  {
+    value: "J",
+    name: "Jack",
+    suit: "Spades",
+    color: "black",
+    image: "/images/blackjack/cards/jack_of_spades.svg",
+  },
+  {
+    value: "Q",
+    name: "Queen",
+    suit: "Spades",
+    color: "black",
+    image: "/images/blackjack/cards/queen_of_spades.svg",
+  },
+  {
+    value: "K",
+    name: "King",
+    suit: "Spades",
+    color: "black",
+    image: "/images/blackjack/cards/king_of_spades.svg",
+  },
+]
